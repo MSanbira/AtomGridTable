570af70d5c40dc6769baaec5a6a0db4bfa4fc0d0
‪Matan Sanbira‬‏ <myt.comp@gmail.com>
2025-07-23 17:15:45 +0200

    refactor[new-page-logic]: Remove filter dependencies from handlers and release stable version
    - Remove CustomFilterDependencies generic from TableHandlers interface
    - Remove filterDependencies from onChange callback parameters
    - Update package version from 1.0.26-canary.4 to stable 1.0.26 release

--- lib/types/table.types.ts
+++ lib/types/table.types.ts
@@ -7,7 +7,7 @@ export interface TableProps<
   CustomFilterDependencies = unknown,
   CustomPaginationApiParams = PaginationApiParams,
   CustomSortingApiParams = SortingApiParams,
-> extends TableHandlers<CustomFilterDependencies, CustomPaginationApiParams, CustomSortingApiParams> {
+> extends TableHandlers<CustomPaginationApiParams, CustomSortingApiParams> {
   colOptions: ColOption[];
   rows: TableRow[];
   className?: string;
@@ -26,13 +26,12 @@ export interface TableProps<
   filterDependencies?: CustomFilterDependencies;
 }
 
-interface TableHandlers<CustomFilterDependencies, CustomPaginationApiParams, CustomSortingApiParams> {
+interface TableHandlers<CustomPaginationApiParams, CustomSortingApiParams> {
   onPageOptionChange?: (pageOptions: PaginationChangeOptions<CustomPaginationApiParams>) => void;
   onSortOptionChange?: (sortOptions: SortingChangeOptions<CustomSortingApiParams>) => void;
   onChange?: (generalOptions: {
     pageOptions: PaginationChangeOptions<CustomPaginationApiParams>;
     sortOptions: SortingChangeOptions<CustomSortingApiParams>;
-    filterDependencies?: CustomFilterDependencies;
   }) => void;
 }
 
